e627adc6f9cd2a5dae589cfbc52d1450e52927fb
Tim Randall <trtimrandall@gmail.com>
2021-06-20 22:49:00 +1000

    Initial Commit

---
+++ CapitalCityQuiz/CapitalCityQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapitalCityQuizApp.swift
+//  CapitalCityQuiz
+//
+//  Created by Tim Randall on 20/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CapitalCityQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CapitalCityQuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CapitalCityQuiz
+//
+//  Created by Tim Randall on 20/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
